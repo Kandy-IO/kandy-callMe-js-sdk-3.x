bfac82fb367822ce84acf0e61027b07b7575f7bb
machine <janwa039@gmail.com>
2019-02-07 17:19:50 +0000

    55744

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js (Next)
  * kandy.callMe.js
- * Version: 3.1.0-beta.54082
+ * Version: 3.2.0-beta.55744
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -51477,7 +51477,8 @@ function* subscribe(connection, credentials, extras = {}) {
       service: connection.service,
       localization: connection.localization || 'English_US',
       useTurn: connection.useTurn || true,
-      notificationType: connection.notificationType || 'WebSocket'
+      notificationType: connection.notificationType || 'WebSocket',
+      supported: ['RingingFeedback']
     }
   });
 
@@ -51803,6 +51804,11 @@ exports.default = [{ name: 'logs', fn: _logs2.default }, { name: 'config', fn: _
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _extends2 = __webpack_require__("../../node_modules/babel-runtime/helpers/extends.js");
+
+var _extends3 = _interopRequireDefault(_extends2);
+
 exports.default = anonymousCalls;
 
 var _index = __webpack_require__("./src/call/oldLink/index.js");
@@ -51820,6 +51826,8 @@ var _events2 = _interopRequireDefault(_events);
 var _actions = __webpack_require__("./src/call/interface/actions/index.js");
 
 var _sagas = __webpack_require__("./src/call/callMe/sagas.js");
+
+var _constants = __webpack_require__("./src/call/constants.js");
 
 var _actions2 = __webpack_require__("./src/events/interface/actions.js");
 
@@ -51853,7 +51861,7 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 
 // Other plugins.
-// TODO: Update when new Link is done.
+// Call plugin.
 function anonymousCalls(options = {}) {
   const defaultOptions = {
     // Values used as defaults for making/answering a call.
@@ -51898,7 +51906,7 @@ function anonymousCalls(options = {}) {
  * @method anonymizeAPI
  * @return {Object} Anonymous Call plugin API.
  */
-// Call plugin.
+// TODO: Update when new Link is done.
 function anonymizeAPI({ dispatch, getState }) {
   // Get the call API.
   let api = _interface2.default.api({ dispatch, getState });
@@ -51990,7 +51998,9 @@ function anonymizeAPI({ dispatch, getState }) {
 
   return {
     // Namespace the API.
-    call: callMeAPI,
+    call: (0, _extends3.default)({}, callMeAPI, {
+      states: _constants.CALL_STATES_FCS
+    }),
     media: mediaAPI
   };
 }
@@ -52129,7 +52139,7 @@ function* anonymousCallEnd() {
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.ICE_MEDIA_STATES = exports.FCS_ICE_MEDIA_STATES = exports.WEBRTC_DEVICE_KINDS = exports.CALL_STATES = exports.CALL_STATES_FCS = exports.FCS_CALL_STATES = undefined;
+exports.ICE_MEDIA_STATES = exports.FCS_ICE_MEDIA_STATES = exports.WEBRTC_DEVICE_KINDS = exports.CALL_DIRECTION = exports.OPERATIONS = exports.CALL_MEDIA_STATES = exports.CALL_STATES = exports.CALL_STATES_FCS = exports.FCS_CALL_STATES = undefined;
 
 var _fp = __webpack_require__("../../node_modules/lodash/fp.js");
 
@@ -52190,6 +52200,44 @@ const CALL_STATES = exports.CALL_STATES = {
   CONNECTED: 'Connected',
   ON_HOLD: 'On Hold',
   ENDED: 'Ended'
+
+  /**
+   * Possible states a call's media can be in.
+   * Describes whether media is flowing or if an endpoint is preventing media flow.
+   * @name CALL_MEDIA_STATES
+   * @type {Object}
+   * @property {string} CONNECTED Both call endpoints allow for media to flow.
+   * @property {string} LOCAL_HOLD Local endpoint disallows media from flowing.
+   * @property {string} REMOTE_HOLD Remote endpoint disallows media from flowing.
+   * @property {string} DUAL_HOLD Both endpoints disallow media from flowing.
+   */
+};const CALL_MEDIA_STATES = exports.CALL_MEDIA_STATES = {
+  CONNECTED: 'Connected',
+  LOCAL_HOLD: 'Local Hold',
+  REMOTE_HOLD: 'Remote Hold',
+  DUAL_HOLD: 'Dual Hold'
+
+  /**
+   * Call operations that require negotiation.
+   * @name OPERATIONS
+   * @type {Object}
+   * @property {string} CHANGE_MEDIA Media flow remains the same, includes non-flow related media changes.
+   * @property {string} HOLD_MEDIA   Media flow stops. May include non-flow related media changes.
+   * @property {string} UNHOLD_MEDIA Media flow restarts. May include non-flow related media changes.
+   */
+};const OPERATIONS = exports.OPERATIONS = {
+  CHANGE_MEDIA: 'Change Media',
+  HOLD_MEDIA: 'Hold Media',
+  UNHOLD_MEDIA: 'Unhold Media'
+
+  /**
+   * Call direction
+   * Indication of which direction the call is going
+   * @name CALL_DIRECTION
+   */
+};const CALL_DIRECTION = exports.CALL_DIRECTION = {
+  INCOMING: 'incoming',
+  OUTGOING: 'outgoing'
 
   /*
    * A conversion from MediaDeviceInfo.kind values to their more common terms.
@@ -58710,6 +58758,7 @@ function autoRestart(saga) {
         // Importing the LogManager in this file breaks tests for an unknown
         //    reason. Should find out why so that we can log in our utils.
         // log.error(`Unhandled error in saga ${saga.name}.`, e)
+        console.log(`Unhandled error in saga ${saga.name}.`, e);
         shouldRestart = true;
       }
     } while (shouldRestart);
@@ -60257,7 +60306,9 @@ const authCodes = exports.authCodes = {
   // A provided parameter is not valid.
   INVALID_PARAM: 'call:7',
   // There is a desync between components' state.
-  STATE_DESYNC: 'call:8'
+  STATE_DESYNC: 'call:8',
+  // Offer could not be generated
+  INVALID_OFFER: 'call:9'
 
   /**
    * Error codes for the Call History plugin.
@@ -61191,7 +61242,7 @@ const factoryDefaults = {
    */
 };function factory(plugins, options = factoryDefaults) {
   // Log the SDK's version (templated by webpack) on initialization.
-  let version = '3.1.0-beta.54082';
+  let version = '3.2.0-beta.55744';
   log.info(`CPaaS SDK version: ${version}`);
 
   var sagas = [];
