b3db56ff62efe706024702e3281198f5eb5bf767
machine <janwa039@gmail.com>
2019-05-22 20:23:35 +0000

    77822

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.callMe.js
- * Version: 3.5.0-beta.77806
+ * Version: 3.5.0-beta.77822
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -15703,7 +15703,7 @@ const factoryDefaults = {
    */
 };function factory(plugins, options = factoryDefaults) {
   // Log the SDK's version (templated by webpack) on initialization.
-  let version = '3.5.0-beta.77806';
+  let version = '3.5.0-beta.77822';
   log.info(`UC SDK version: ${version}`);
 
   var sagas = [];
