f2d7e57b625b2a70d27818ae068b11b5e6589d95
machine <janwa039@gmail.com>
2021-03-17 11:59:10 +0000

    631

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.callMe.js
- * Version: 3.26.0-beta.630
+ * Version: 3.26.0-beta.631
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -59809,7 +59809,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '3.26.0-beta.630';
+  return '3.26.0-beta.631';
 }
 
 /***/ }),
