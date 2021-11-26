82b9593503cdc0d1874b00e0abb5f6ecf45c7c08
machine <janwa039@gmail.com>
2021-11-26 21:36:45 +0000

    798

--- dist/kandy.js
+++ dist/kandy.js
@@ -1,7 +1,7 @@
 /**
  * Kandy.js
  * kandy.callMe.js
- * Version: 3.34.0-beta.797
+ * Version: 3.34.0-beta.798
  */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
@@ -11450,7 +11450,7 @@ exports.getVersion = getVersion;
  * for the @@ tag below with actual version value.
  */
 function getVersion() {
-  return '3.34.0-beta.797';
+  return '3.34.0-beta.798';
 }
 
 /***/ }),
